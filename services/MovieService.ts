12de7092a3425d465d4b234763b1864110744b82
Anas <anascoder4@gmail.com>
2024-11-11 13:14:30 +0530

    fixex

--- services/MovieService.ts
+++ services/MovieService.ts
@@ -3,6 +3,7 @@ import { S3Client, PutObjectCommand   } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { title } from "process";
 
+//  my s3 client
 const client = new S3Client({
     region: process.env.AWS_REGION as string ,
     credentials: {

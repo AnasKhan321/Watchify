8fd885a34177daa702c28b1df9973aa33f1508b2
Anas <anascoder4@gmail.com>
2024-12-15 22:56:22 +0530

    changes

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,15 +1,40 @@
-import MovieDetail from "@/app/clientComponents/MovieDetail";
-import axios from "axios";
+import MovieDetail from "@/app/clientComponents/MovieDetail"
+import {  Movie } from "@/interfaces"
 
-export default async function Page({params}  : { params: Promise<{ id: string }>}){
-    const parameters = await params ; 
 
-    const {data }  = await  axios.get(`https://watchify-topaz.vercel.app/api/movie/${parameters.id}`)
+interface  Movies{
+    movies : Movie[]
+}
 
+interface MovieDetails{
+    data : Movie
+}
+
+
+ 
+export async function generateStaticParams() {
+  const response :Movies  = await fetch('https://watchify-topaz.vercel.app/api/movie'  ,    { next: { revalidate: 60 * 60 * 24 }}).then((res) =>
+    res.json()
+  )
+  const data  : Movie[] = response.movies
+  return data.map((Web) => ({
+    id: String(Web.id),
+  }))
+}
+ 
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const id = (await params).id
+  const data: MovieDetails = await fetch(`https://watchify-topaz.vercel.app/api/movie/${id}`  , { next: { revalidate: 60 * 60 * 24 }}).then(
+    (res) => res.json()
+  )
 
     if(data.data==null){
         return (
-            <div className="text-center font-bold text-2xl  mt-10 ">Movie not found !</div>
+            <div className="text-center font-bold text-2xl  mt-10 ">Web not found !</div>
         )
     }
 

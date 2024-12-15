c5630acc92e1d83022312939b412589c361a4fca
Anas <anascoder4@gmail.com>
2024-12-15 22:38:08 +0530

    changes

--- app/api/movie/route.ts
+++ app/api/movie/route.ts
@@ -1,5 +1,4 @@
 import { MovieService } from "@/services/MovieService";
-import { NextRequest } from "next/server";
 
 export  async function POST(request : Request ){
     try {
--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,38 +1,11 @@
-import MovieDetail from "@/app/clientComponents/MovieDetail"
-import { Movie } from "@/interfaces"
+import MovieDetail from "@/app/clientComponents/MovieDetail";
+import axios from "axios";
 
+export default async function Page({params}  : { params: Promise<{ id: string }>}){
+    const parameters = await params ; 
 
-interface  Movies{
-    movies : Movie[]
-}
+    const {data }  = await  axios.get(`https://watchify-topaz.vercel.app/api/movie/${parameters.id}`)
 
-interface MovieDetail{
-    data : Movie
-}
-
-export const revalidate = 60*60*24
- 
-export const dynamicParams = true 
- 
-export async function generateStaticParams() {
-  const response :Movies  = await fetch('https://watchify-topaz.vercel.app/api/movie').then((res) =>
-    res.json()
-  )
-  const data  : Movie[] = response.movies
-  return data.map((movie) => ({
-    id: String(movie.id),
-  }))
-}
- 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>
-}) {
-  const id = (await params).id
-  const data: MovieDetail = await fetch(`https://watchify-topaz.vercel.app/api/movie/${id}`).then(
-    (res) => res.json()
-  )
 
     if(data.data==null){
         return (
